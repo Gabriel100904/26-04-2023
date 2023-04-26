661e89e61ad5e1ff442a0694ef2999c83f5107a7
Gabriel100904 <99267071+Gabriel100904@users.noreply.github.com>
2023-04-26 11:10:25 -0300

    Add files via upload

---
+++ ifunny/components/AssetExample.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/IFunny_Logo.png')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black'
+    
+  },
+  logo: {
+    height: 200,
+    width: 300,
+    marginBottom: 100
+  }
+});
