11750b39b59ea5b364874b923279740ccc380925
Gabriel100904 <99267071+Gabriel100904@users.noreply.github.com>
2023-04-27 12:42:56 -0300

    Add files via upload

---
+++ ifunny-2/components/AssetExample.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/meme.jpg')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  logo: {
+    height: 350,
+    width: 300,
+  },
+  
+});
